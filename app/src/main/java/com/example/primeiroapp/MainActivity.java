4efc6543326f8393c0f39a019e33d6300d9cc06d
Bruno Geraldo <brunogeraldo2011@gmail.com>
2023-03-31 13:28:58 -0300

    Implementação do layout de lista

--- app/src/main/java/com/example/primeiroapp/MainActivity.java
+++ app/src/main/java/com/example/primeiroapp/MainActivity.java
@@ -2,16 +2,29 @@ package com.example.primeiroapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Jose"));
+        TextView primeiroAluno = findViewById(R.id.textView3);
+        TextView segundoAluno = findViewById(R.id.textView4);
+        TextView terceiroAluno = findViewById(R.id.textView6);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText((alunos.get(2)));
     }
 }
 //teste de commit simples
