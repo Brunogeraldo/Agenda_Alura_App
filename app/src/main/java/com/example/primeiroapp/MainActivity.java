bea19a9a7c6f462da192e9ee421439e8c40cda90
Bruno Geraldo <brunogeraldo2011@gmail.com>
2023-03-31 14:06:57 -0300

    AdapterViews para coleção de views

--- app/src/main/java/com/example/primeiroapp/MainActivity.java
+++ app/src/main/java/com/example/primeiroapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.primeiroapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,19 +14,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(
-                Arrays.asList("Alex", "Fran", "Jose"));
-        TextView primeiroAluno = findViewById(R.id.textView3);
-        TextView segundoAluno = findViewById(R.id.textView4);
-        TextView terceiroAluno = findViewById(R.id.textView6);
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText((alunos.get(2)));
+                Arrays.asList("Alex", "Fran", "Jose", "maria", "josé"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
 //teste de commit simples
