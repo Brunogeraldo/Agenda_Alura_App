9c5bbc0488740ac248b0792f6f9c12103a2c543d
Bruno Geraldo <brunogeraldo2011@gmail.com>
2023-03-31 11:38:35 -0300

    Implementação do layout da Activity

--- app/src/main/java/com/example/primeiroapp/MainActivity.java
+++ app/src/main/java/com/example/primeiroapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.primeiroapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -10,7 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Bruno", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
     }
 }
 //teste de commit simples
