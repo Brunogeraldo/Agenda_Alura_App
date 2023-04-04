dcca71c3722597e4bdd21f1dfc582c0e27f897ec
Bruno Geraldo <brunogeraldo2011@gmail.com>
2023-04-04 09:45:27 -0300

    Adição de app bar

--- app/src/main/java/com/example/primeiroapp/MainActivity.java
+++ app/src/main/java/com/example/primeiroapp/MainActivity.java
@@ -9,21 +9,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de alunos");
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Alex", "Fran", "Jose", "maria", "josé"));
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_alunos);
-        listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
 //teste de commit simples
