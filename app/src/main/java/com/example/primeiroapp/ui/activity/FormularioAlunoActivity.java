8e79730fc1b0a08c32c8fe2091f8c12d9ee91f56
Bruno Geraldo <brunogeraldo2011@gmail.com>
2023-04-12 11:03:48 -0300

    Nova activity e refatorando a main

---
+++ app/src/main/java/com/example/primeiroapp/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package com.example.primeiroapp.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.primeiroapp.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
--- app/src/main/java/com/example/primeiroapp/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/primeiroapp/ui/activity/ListaAlunosActivity.java
@@ -1,30 +1,28 @@
-package com.example.primeiroapp;
+package com.example.primeiroapp.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.primeiroapp.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de alunos");
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Alex", "Fran", "Jose", "maria", "josé"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
