1abf91dc88d232289ab3ba02f1219fc057e0c45e
Bruno Geraldo <brunogeraldo2011@gmail.com>
2023-04-14 10:42:28 -0300

    refatoracao

--- app/src/main/java/com/example/primeiroapp/dao/AlunoDAO.java
+++ app/src/main/java/com/example/primeiroapp/dao/AlunoDAO.java
@@ -4,13 +4,11 @@ import com.example.primeiroapp.model.Aluno;
 
 import java.util.ArrayList;
 import java.util.List;
-
 public class AlunoDAO {
     private final static List<Aluno> alunos = new ArrayList<>();
     public void salva(Aluno aluno) {
         alunos.add(aluno);
     }
-
     public List<Aluno> todos() {
         return new ArrayList<>(alunos);
     }
